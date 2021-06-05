9f75bc4e9bcf1d14f615288323c0777e6b1875c7
ncougs <ncougan@gmail.com>
2021-06-05 12:51:39 +1000

    adding max length function & ranom uppercase function

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,62 @@
 // Assignment Code
-console.log("test test")
+
 var generateBtn = document.querySelector("#generate");
 
+// Generate random password function, return result
 function generatePassword () {
-  return "password12345"
+
+
+// set character arrays
+ var alphaCharacters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
+ var numberCharacters = "0 1 2 3 4 5 6 7 8 9".split(" ");
+ var specialCharacters = "! @ # $ % ^ & * ( ) _ +".split(" ");
+ var maxLenth;
+
+ // Get max length of the array
+function getMaxLenth () {
+  var result = window.prompt("How many characters would you like in the Password? (Minimum of 8 characters and maximum of 128 characters")
+  return result
+}
+
+maxLenth = getMaxLenth()
+
+function 
+
+// Generate random uppercase characters from alphaCharacters array
+  function randomUppercase () {
+    // Declare random true of false variable.
+    var randomBoolean;
+   
+    // For each character in the alphaCharacters array, assign a random uppercase of lowercase
+    for (counter = 0; counter < alphaCharacters.length; counter++) {
+     var randomBoolean = Math.random() < 0.5;
+       if(randomBoolean) {
+           alphaCharacters[counter] = alphaCharacters[counter].toUpperCase(); console.log(alphaCharacters[counter]);
+       }
+       else {
+         alphaCharacters[counter] = alphaCharacters[counter].toLocaleLowerCase(); console.log(alphaCharacters[counter]);
+       }
+    }
+   
+    return alphaCharacters
+   }
+
+
+
+
+
+ // join the arrays together for the global password array
+ var globalCharaters = alphaCharacters.concat(numberCharacters,specialCharacters); 
+ 
+ // Declare random string to be "".
+ var random ="";  
+ 
+// Randdom = random + new random charater from global array, loop until max length of charactes has bene met
+for (var counter=0; counter < maxLenth; counter++) {
+  random = random + globalCharaters[Math.floor(Math.random() * globalCharaters.length)];
+ };
+
+return random;
 }
 
 // Write password to the #password input
