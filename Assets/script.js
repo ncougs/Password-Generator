8dba193f1a6801b8dd2fef8dcb7895d430bb850e
ncougs <ncougan@gmail.com>
2021-06-05 10:30:44 +1000

    Adding basic HTML, JS & CSS strucutre

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+console.log("test test")
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+  return "password12345"
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
